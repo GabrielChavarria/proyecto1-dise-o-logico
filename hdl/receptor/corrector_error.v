3a66c6ce78cb64c62d46cdc5f42f0314575791fa
gabriel chavarria <tu_correo@gmail.comg>
2026-03-24 19:06:50 -0600

    Modulos 7.1 y 7.2

---
+++ hdl/receptor/corrector_error.v
@@ -0,0 +1,17 @@
+// Módulo 7.2: Corrector de error
+module corrector_error (
+    input [6:0] rx,          // datos recibidos
+    input [2:0] error_pos,   // posición del error detectado
+    output reg [6:0] corrected // datos corregidos
+);
+
+    always @(*) begin
+        corrected = rx; // copiar datos
+
+        // si hay error, corregir
+        if (error_pos != 3'b000) begin
+            corrected[error_pos - 1] = ~rx[error_pos - 1];
+        end
+    end
+
+endmodule
