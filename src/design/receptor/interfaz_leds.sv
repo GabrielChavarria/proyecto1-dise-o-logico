6e374876583e28d664ed6c5c10a7458da1cf50c8
KeeperMitch <marmorales@estudiantec.cr>
2026-04-05 13:25:03 -0600

    modificaciones

--- src/design/receptor/interfaz_leds.sv
+++ src/design/receptor/interfaz_leds.sv
@@ -1,17 +1,13 @@
 // Sección 7.3: interfaz de LEDs (Receptor/ Salida)
-// Descripción: Este módulo se encarga de tomar el dato procesado por el receptor y mostrarlo en los LEDs.
 module interfaz_leds (
-    input logic [3:0] data_in, // Dato de 4 bits que se mostrará en los LEDs
-    output logic [3:0] leds_out  // Salida para los LEDs
+    input  [3:0] data_in,  // Dato de 4 bits que entra
+    output [3:0] leds_out  // Salida para los LEDs
 );
 
-// El enunciado pide que la lógica se escriba en ecaciones booleanas, por lo que se asigna directamente 
-//la entrada a la salida.
+// Se asigna la entrada a la salida usando ecuaciones de identidad.
+// Usamos los nombres exactos que definimos arriba (data_in y leds_out).
 
-assing codigo_bin_led_po[0]= datos_corregidos[0];
-assign codigo_bin_led_po[1]= datos_corregidos[1];
-assign codigo_bin_led_po[2]= datos_corregidos[2];       
-assign codigo_bin_led_po[3]= datos_corregidos[3];
+assign leds_out = data_in;
 
 endmodule
 
