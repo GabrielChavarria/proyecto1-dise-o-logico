5dcfb1f5c96d098135bb2cb08b5a199dc5b75f69
Andy2335 <andres.obregon23@gmail.com>
2026-04-15 14:46:46 -0600

    Cambio de pines

--- src/design/receptor/decodificador_7seg.sv
+++ src/design/receptor/decodificador_7seg.sv
@@ -7,23 +7,23 @@ module decodificador_7seg (
 
     always_comb begin
         case (bin_in)
-            4'h0: seg_out = 7'b0111111; // 0
-            4'h1: seg_out = 7'b0000110; // 1
-            4'h2: seg_out = 7'b1011011; // 2
-            4'h3: seg_out = 7'b1001111; // 3
-            4'h4: seg_out = 7'b1100110; // 4 
-            4'h5: seg_out = 7'b1101101; // 5
-            4'h6: seg_out = 7'b1111101; // 6
+            4'h0: seg_out = 7'b0000000; // 0
+            4'h1: seg_out = 7'b0000001; // 1
+            4'h2: seg_out = 7'b0000010; // 2
+            4'h3: seg_out = 7'b0000011; // 3
+            4'h4: seg_out = 7'b0000100; // 4 
+            4'h5: seg_out = 7'b0000101; // 5
+            4'h6: seg_out = 7'b0000110; // 6
             4'h7: seg_out = 7'b0000111; // 7
-            4'h8: seg_out = 7'b1111111; // 8
-            4'h9: seg_out = 7'b1101111; // 9
-            4'hA: seg_out = 7'b1110111; // A
-            4'hB: seg_out = 7'b1111100; // B
-            4'hC: seg_out = 7'b0111001; // C
-            4'hD: seg_out = 7'b1011110; // D
-            4'hE: seg_out = 7'b1111001; // E
-            4'hF: seg_out = 7'b1110001; // F
-            default: seg_out = 7'b0111111; // Mostrar 0 por defecto
+            4'h8: seg_out = 7'b0001000; // 8
+            4'h9: seg_out = 7'b0001001; // 9
+            4'hA: seg_out = 7'b0001010; // A
+            4'hB: seg_out = 7'b0001011; // B
+            4'hC: seg_out = 7'b0001100; // C
+            4'hD: seg_out = 7'b0001101; // D
+            4'hE: seg_out = 7'b0001110; // E
+            4'hF: seg_out = 7'b0001111; // F
+            default: seg_out = 7'b0000000; // Mostrar 0 por defecto
         endcase
     end
 
